ccb05878eb3567604e877bddcc000f128bc960d5
john <zhangshuai11241@163.com>
2019-03-22 13:53:17 +0800

    C ++

---
+++ CProject/CProject/Helper.hpp
@@ -0,0 +1,14 @@
+//
+//  Helper.hpp
+//  CProject
+//
+//  Created by John on 2019/3/22.
+//  Copyright © 2019 John. All rights reserved.
+//
+
+#ifndef Helper_hpp
+#define Helper_hpp
+
+#include <stdio.h>
+
+#endif /* Helper_hpp */
---
+++ CProject/CProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CProject
+//
+//  Created by John on 2019/3/22.
+//  Copyright © 2019 John. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
